3dbc17daa782703b0a754d507c059e4f1b9ff67a
John Cardwell <cardwell.john@gmail.com>
2016-10-25 17:20:39 -0700

    add decoration and scroll to top when page is loaded

---
+++ content/application.js
@@ -0,0 +1,7 @@
+/**
+ * Created by johncardwell on 10/25/16.
+ */
+
+$(function() {
+    $('html,body').animate({ scrollTop: 420 }, 'slow');
+});
---
+++ output/application.js
@@ -0,0 +1,8 @@
+/**
+ * Created by johncardwell on 10/25/16.
+ */
+
+$(function() {
+    console.log( "ready!" );
+    $('html,body').animate({ scrollTop: 420 }, 'slow');
+});
